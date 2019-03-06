bbb1c358c41624c412049015842e63ce25a3250f
huyphamkma <huyphamkma@gmail.com>
2019-03-06 09:32:20 +0700

    main function

---
+++ app/src/main/java/huy/tablayout/PagerAdapter.java
@@ -0,0 +1,50 @@
+package huy.tablayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new OneFragment();
+                break;
+            case 1:
+                fragment = new TwoFragment();
+                break;
+            case 2:
+                fragment = new ThreeFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position){
+            case 0:
+                title = "One";
+                break;
+            case 1:
+                title = "Two";
+                break;
+            case 2:
+                title = "Three";
+                break;
+        }
+        return title;
+    }
+}
